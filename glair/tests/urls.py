14c64cae107c90c439cd7370516c316cf9c16cac
Matthew McCants <mattmccants@gmail.com>
2014-05-27 11:17:04 -0500

    Move api url routing to app-level urls.py.

--- glair/tests/urls.py
+++ glair/tests/urls.py
@@ -2,16 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import viewsets, routers
-
-from glair.views import BasicPhotoViewset
-
-router = routers.DefaultRouter()
-router.register(r'photos', BasicPhotoViewset)
 
 
 urlpatterns = patterns('',
 	url(r'^glair/', include('glair.urls')),
-	url(r'^api/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- glair/urls.py
+++ glair/urls.py
@@ -1,11 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 
-from .views import PhotoDetail, UploadView, PhotoBasicEdit
+from rest_framework import viewsets, routers
 
+from .views import (PhotoDetail, UploadView, PhotoBasicEdit, BasicPhotoViewset)
+
+router = routers.DefaultRouter()
+router.register(r'photos', BasicPhotoViewset)
 
 urlpatterns = patterns('',
+	url(r'^api/', include(router.urls)),
 	url(r'^photo/(?P<pk>\d+)-(?P<slug>[-\w]+)/$', PhotoDetail.as_view(), name='photo-detail'),
 	url(r'^photo/edit/(?P<pk>\d+)/$', PhotoBasicEdit.as_view(), name='photo-basic-edit'),
 	url(r'^upload/', login_required(UploadView.as_view()), name='glair-upload'),
