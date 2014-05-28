a93bfb14d9dc9b8bd5b7becc467c116876c9e9cb
Matthew McCants <mattmccants@gmail.com>
2014-05-27 20:29:33 -0500

    Using django-rest-api

--- glair/views.py
+++ glair/views.py
@@ -1,11 +1,9 @@
-import json
-
 import logging
 
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView, UpdateView
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.core.urlresolvers import reverse
 
 from django.template import Context
@@ -14,6 +12,8 @@ from crispy_forms.utils import render_crispy_form
 
 from rest_framework import viewsets
 from rest_framework import permissions
+
+from sorl.thumbnail import get_thumbnail
 
 from .models import Photo
 from .forms import UploadForm, BasicEditForm
@@ -32,19 +32,16 @@ class UploadView(FormView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         image = form.save()
-        edit_form = BasicEditForm(initial={ 'name': image.name, 
-                                            'is_private': image.is_private,
-                                            'tags': image.tags,
-                                            'description': image.description})
 
         template = get_template('glair/photo_inline_edit.html')
-        html = template.render(Context({'object': image, 'form': edit_form}))
-        return HttpResponse(json.dumps({'pk': image.pk, 'html': html}), content_type='application/json')
+        html = template.render(Context({'object': image}))
+
+        return JsonResponse({
+            'pk': image.pk, 
+            'html': html
+        })
 
 class BasicPhotoViewset(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Photo.objects.all()
     serializer_class = BasicPhotoSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
